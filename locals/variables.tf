781704b0d14781c1b09d8f67f7a51837082c7624
samandhalapradeep <pradeepsamandhala7@gmail.com>
2024-07-16 13:53:09 +0530

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+  default = "Z00838311Q9EXSVA8UNJP"
+}
+
+variable "domain_name" {
+  default = "pradeepstore.online"
+}
